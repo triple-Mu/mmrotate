e6606f1eee578fcd0bacac3b5d6e3506a8f8b41c
triplemu <gpu@163.com>
2024-04-07 19:38:37 +0800

    add cache-copy-paste

---
+++ configs/rotated_rtmdet/rotated_rtmdet_l-comp.py
@@ -0,0 +1,169 @@
+_base_ = ['./_base_/default_runtime.py', './_base_/schedule_3x.py']
+
+# coco_ckpt = 'rtmdet_l_8xb32-300e_coco_20220719_112030-5a0be7c4.pth'  # noqa
+load_from = 'epoch_300.pth'
+resume = None
+
+data_root = 'data/DOTA/'
+dataset_type = 'DOTADataset'
+metainfo = {
+    'classes':
+        ('A', 'B', 'C', 'D', 'E', 'F'),
+    'palette': [(165, 42, 42), (189, 183, 107), (0, 255, 0),
+                (255, 0, 0), (138, 43, 226), (255, 128, 0)]
+}
+
+file_client_args = dict(backend='disk')
+angle_version = 'le90'
+
+batch_size = 16
+num_workers = 0
+
+model = dict(
+    type='mmdet.RTMDet',
+    data_preprocessor=dict(
+        type='mmdet.DetDataPreprocessor',
+        mean=[103.53, 116.28, 123.675],
+        std=[57.375, 57.12, 58.395],
+        bgr_to_rgb=False,
+        boxtype2tensor=False,
+        batch_augments=None),
+    backbone=dict(
+        type='mmdet.CSPNeXt',
+        arch='P5',
+        expand_ratio=0.5,
+        deepen_factor=1,
+        widen_factor=1,
+        channel_attention=True,
+        norm_cfg=dict(type='SyncBN'),
+        act_cfg=dict(type='SiLU'),
+        # init_cfg=dict(
+        #     type='Pretrained', prefix='backbone.', checkpoint=coco_ckpt)
+    ),
+    neck=dict(
+        type='mmdet.CSPNeXtPAFPN',
+        in_channels=[256, 512, 1024],
+        out_channels=256,
+        num_csp_blocks=3,
+        expand_ratio=0.5,
+        norm_cfg=dict(type='SyncBN'),
+        act_cfg=dict(type='SiLU'),
+        # init_cfg=dict(
+        #     type='Pretrained', prefix='neck.', checkpoint=coco_ckpt)
+    ),
+    bbox_head=dict(
+        type='RotatedRTMDetSepBNHead',
+        num_classes=6,
+        in_channels=256,
+        stacked_convs=2,
+        feat_channels=256,
+        angle_version=angle_version,
+        anchor_generator=dict(
+            type='mmdet.MlvlPointGenerator', offset=0, strides=[8, 16, 32]),
+        bbox_coder=dict(
+            type='DistanceAnglePointCoder', angle_version=angle_version),
+        loss_cls=dict(
+            type='mmdet.QualityFocalLoss',
+            use_sigmoid=True,
+            beta=2.0,
+            loss_weight=1.0),
+        loss_bbox=dict(type='RotatedIoULoss', mode='linear', loss_weight=2.0),
+        with_objectness=False,
+        exp_on_reg=True,
+        share_conv=True,
+        pred_kernel_size=1,
+        use_hbbox_loss=False,
+        scale_angle=False,
+        loss_angle=None,
+        norm_cfg=dict(type='SyncBN'),
+        act_cfg=dict(type='SiLU'),
+        # init_cfg=dict(
+        #     type='Pretrained', prefix='bbox_head.', checkpoint=coco_ckpt)
+    ),
+    train_cfg=dict(
+        assigner=dict(
+            type='mmdet.DynamicSoftLabelAssigner',
+            iou_calculator=dict(type='RBboxOverlaps2D'),
+            topk=13),
+        allowed_border=-1,
+        pos_weight=-1,
+        debug=False),
+    test_cfg=dict(
+        nms_pre=2000,
+        min_bbox_size=0,
+        score_thr=0.05,
+        nms=dict(type='nms_rotated', iou_threshold=0.1),
+        max_per_img=2000),
+)
+
+train_pipeline = [
+    dict(type='mmdet.LoadImageFromFile', file_client_args=file_client_args),
+    dict(type='mmdet.LoadAnnotations', with_bbox=True, box_type='qbox'),
+    dict(type='ConvertBoxType', box_type_mapping=dict(gt_bboxes='rbox')),
+    dict(type='CacheCopyPaste', num_copy_thres=20, max_capacity=1024),
+    dict(type='mmdet.Resize', scale=(1024, 1024), keep_ratio=True),
+    dict(
+        type='mmdet.RandomFlip',
+        prob=0.75,
+        direction=['horizontal', 'vertical', 'diagonal']),
+    dict(
+        type='RandomRotate',
+        prob=0.5,
+        angle_range=180,
+        rect_obj_labels=[9, 11]),
+    dict(
+        type='mmdet.Pad', size=(1024, 1024),
+        pad_val=dict(img=(114, 114, 114))),
+    dict(type='mmdet.PackDetInputs')
+]
+
+val_pipeline = [
+    dict(type='mmdet.LoadImageFromFile', file_client_args=file_client_args),
+    dict(type='mmdet.Resize', scale=(1024, 1024), keep_ratio=True),
+    # avoid bboxes being resized
+    dict(type='mmdet.LoadAnnotations', with_bbox=True, box_type='qbox'),
+    dict(type='ConvertBoxType', box_type_mapping=dict(gt_bboxes='rbox')),
+    dict(
+        type='mmdet.Pad', size=(1024, 1024),
+        pad_val=dict(img=(114, 114, 114))),
+    dict(
+        type='mmdet.PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
+                   'scale_factor'))
+]
+
+train_dataloader = dict(
+    batch_size=batch_size,
+    num_workers=num_workers,
+    persistent_workers=num_workers > 0,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    batch_sampler=None,
+    pin_memory=False,
+    dataset=dict(
+        type=dataset_type,
+        metainfo=metainfo,
+        data_root=data_root,
+        ann_file='train/annfiles/',
+        data_prefix=dict(img_path='train/images/'),
+        filter_cfg=dict(filter_empty_gt=True),
+        pipeline=train_pipeline))
+
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=num_workers // 2,
+    persistent_workers=(num_workers // 2) > 0,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        metainfo=metainfo,
+        data_root=data_root,
+        ann_file='val/annfiles/',
+        data_prefix=dict(img_path='val/images/'),
+        test_mode=True,
+        pipeline=val_pipeline))
+
+test_dataloader = val_dataloader
+
+val_evaluator = dict(type='DOTAMetric', metric='mAP')
+test_evaluator = val_evaluator
--- mmrotate/datasets/transforms/__init__.py
+++ mmrotate/datasets/transforms/__init__.py
@@ -1,9 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .loading import LoadPatchFromNDArray
 from .transforms import (ConvertBoxType, ConvertMask2BoxType,
-                         RandomChoiceRotate, RandomRotate, Rotate)
+                         RandomChoiceRotate, RandomRotate, Rotate, CacheCopyPaste)
 
 __all__ = [
     'LoadPatchFromNDArray', 'Rotate', 'RandomRotate', 'RandomChoiceRotate',
-    'ConvertBoxType', 'ConvertMask2BoxType'
+    'ConvertBoxType', 'ConvertMask2BoxType', 'CacheCopyPaste'
 ]
