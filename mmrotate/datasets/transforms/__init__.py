e531d499983a33acba3d488b3b67fae8f7c3e360
triplemu <gpu@163.com>
2024-04-20 13:20:31 +0800

    fix

--- mmrotate/datasets/transforms/__init__.py
+++ mmrotate/datasets/transforms/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .loading import LoadPatchFromNDArray
-from .transforms import (ConvertBoxType, ConvertMask2BoxType,
-                         RandomChoiceRotate, RandomRotate, Rotate, CacheCopyPaste)
+from .transforms import (CacheCopyPaste, ConvertBoxType, ConvertMask2BoxType,
+                         RandomChoiceRotate, RandomRotate, Rotate)
 
 __all__ = [
     'LoadPatchFromNDArray', 'Rotate', 'RandomRotate', 'RandomChoiceRotate',
